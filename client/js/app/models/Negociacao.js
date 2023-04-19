0f2ad7d2cdcd4883ba52b52ad9dbb40707e95641
alexandrejs777 <alexandrejatobasiqueira@gmail.com>
2023-04-19 20:36:38 -0300

    Construtor da classe e métodos

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,14 @@
 class Negociacao {
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0;
+
+    // Construtor
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    // Método
+    obtemVolume() {
+        return this.quantidade * this.valor;
     }
 }
